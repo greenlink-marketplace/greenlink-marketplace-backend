fb0891a1e1e50f0f7b9ebe688ce9ca6c693141ad
rodrig-crzz <rodrigow1238@gmail.com>
2025-05-23 23:33:36 -0300

    Atuali. Config.

--- greenlink_backend/settings.py
+++ greenlink_backend/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'core',
+    'marketplace',
+    'recycling'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media/")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
