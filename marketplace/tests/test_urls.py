48cf6dac0cf87b9e58b7a7c31ffcac74b9bcf65c
rodrig-crzz <rodrigow1238@gmail.com>
2025-05-27 13:38:17 -0300

    test(marketplace): testes ini. p/ cadastro de consumidores

---
+++ marketplace/tests/test_urls.py
@@ -0,0 +1,65 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class RegisterConsumerPostTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("consumer-register")
+        self.valid_payload = {
+            "username": "user_test",
+            "email": "emailtest@gmail.com",
+            "password": "12345678",
+            "first_name": "User",
+            "last_name": "Test",
+            "cpf": "123.456.789-09",
+            "phone": "(00)92761-2097",
+            "address": "address test for post"
+        }
+
+    def test_with_cpf_invalid(self):
+        payload = self.valid_payload.copy()
+        
+        invalid_cpfs = [
+            "000.000.000-00",
+            "999.999.999-99",
+            "12345678909",   
+            "123.456.789-00",
+            "111.111.111-11",
+            "abc.def.ghi-jk",
+            "",
+            # None,              # Inválido (valor nulo)
+            12345678909,
+            "123.456.78-909",
+        ]
+        
+        for invalid_cpf in invalid_cpfs:
+            payload["cpf"] = invalid_cpf
+            response = self.client.post(self.url, payload, format="Json")
+            self.assertEqual(response.status_code, 400)
+
+    def test_with_email_invalid(self):
+        payload = self.valid_payload.copy()
+        
+        invalid_emails = [
+            "plainaddress",
+            "@semusuario.com",
+            "usuario@",
+            "usuario@.com",
+            "usuario@com",
+            "usuario@com.",
+            "usuario@.com.",
+            "",
+            # did not pass the test
+                # "usua rio@exemplo.com",
+                # "usuario@@exemplo.com",
+                # "usuario@exemplo..com",
+                # "usuario@-exemplo.com",
+                # "usuario@exemplo-.com",
+                # "usuario@exemplo.c",
+                # "usuario@exemplo.toolongtld,
+        ]
+        
+        for invalid_email in invalid_emails:
+            payload["email"] = invalid_email
+            response = self.client.post(self.url, payload, format="Json")
+            self.assertEqual(response.status_code, 400)
