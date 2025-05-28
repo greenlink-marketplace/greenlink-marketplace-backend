da9d89a262483954e6b4388232adc44ef8f54daf
rodrig-crzz <rodrigow1238@gmail.com>
2025-05-28 17:18:34 -0300

    Config. Django Admin apps

--- recycling/admin.py
+++ recycling/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# FIXME: This is not the best practice for registering models in
+#  the Django admin. Automatically registering all models may expose
+#  models that should remain hidden or uneditable. It's better to
+#  explicitly register only the necessary models with custom admin
+#  configurations if needed.
+app_models = apps.get_app_config('recycling').get_models()
+
+for model in app_models:
+    admin.site.register(model)
