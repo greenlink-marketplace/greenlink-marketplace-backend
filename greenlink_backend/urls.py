a9e39eff9a7bb13f229ba54c9777802b5926df1b
rodrig-crzz <rodrigow1238@gmail.com>
2025-05-28 19:14:12 -0300

    Organ. admin/ & api/

--- greenlink_backend/urls.py
+++ greenlink_backend/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+api_urlpatterns = [
+    path("accounts/", include("accounts.urls")),
+    path("marketplace/", include("marketplace.urls")),
+    # path("core/", include("core.urls")),
+    # path("recycling/", include("recycling.urls")),
+]
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
-    path("api/marketplace/", include("marketplace.urls")),
+    path("admin/", admin.site.urls),
+    path("api/", include(api_urlpatterns)),
 ]
