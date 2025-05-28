c87d14ae6daf15c67ed641f2e57f15a4bacc030e
rodrig-crzz <rodrigow1238@gmail.com>
2025-05-28 20:32:38 -0300

    fix. exportaçao

--- greenlink_backend/settings.py
+++ greenlink_backend/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from decouple import config
 from datetime import timedelta
-from corsheaders import _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
