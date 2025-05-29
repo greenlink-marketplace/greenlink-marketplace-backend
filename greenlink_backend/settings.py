cbf55c7623bcd6b60fca057ab9747c9bfc9e5660
rodrig-crzz <rodrigow1238@gmail.com>
2025-05-29 11:44:33 -0300

    Config. backend de autentif.

--- greenlink_backend/settings.py
+++ greenlink_backend/settings.py
@@ -118,6 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailOrUsernameBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
