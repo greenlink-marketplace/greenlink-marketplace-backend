0c059668816276aaf18f5bf89ac28e6e0c9421f8
rodrig-crzz <rodrigow1238@gmail.com>
2025-06-12 09:35:23 -0300

    Mod. do modelo de Locais de Coleta p/ guardar valores de latitude e longitude

--- recycling/models.py
+++ recycling/models.py
@@ -34,6 +34,8 @@ class RecyclingLocation(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
     contact = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return self.name
